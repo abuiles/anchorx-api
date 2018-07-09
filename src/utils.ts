c8de35a1f455d1c385aeec607d29b16e3c9c89ff
Adolfo Builes <builes.adolfo@gmail.com>
2018-07-09 12:42:43 -0500

    Move create account in ledger to utils.

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,44 @@
+import {
+  Keypair,
+  Network,
+  Operation,
+  Server,
+  TransactionBuilder
+} from 'stellar-sdk'
+
 import { Prisma } from './generated/prisma'
 
 export interface Context {
   db: Prisma
   request: any
+}
+
+export async function createAccountInLedger(newAccount) {
+  try {
+    Network.useTestNetwork();
+    const stellarServer = new Server('https://horizon-testnet.stellar.org');
+
+    // Never put values like the an account seed in code.
+    const provisionerKeyPair = Keypair.fromSecret('SA72TGXRHE26WC5G5MTNURFUFBHZHTIQKF5AQWRXJMJGZUF4XY6HFWJ4')
+    const issuerKeyPair = Keypair.fromSecret('SBYZ5NEJ34Y3FTKADVBO3Y76U6VLTREJSW4MXYCVMUBTL2K3V4Y644UX')
+
+    const provisioner = await stellarServer.loadAccount(provisionerKeyPair.publicKey())
+
+    console.log('creating account in ledger', newAccount)
+
+    const transaction = new TransactionBuilder(provisioner)
+      .addOperation(
+        Operation.createAccount({
+          destination: newAccount,
+          startingBalance: '2'
+        })
+      ).build()
+
+    transaction.sign(provisionerKeyPair)
+
+    const result = await stellarServer.submitTransaction(transaction);
+    console.log('Account created: ', result)
+  } catch (e) {
+    console.log('Stellar account not created.', e)
+  }
 }
